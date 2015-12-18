6edd12f08ce9ae07481d2e1a6302c98e5201ccb9
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2015-12-18 10:15:23 +0100

    Create a model

--- js/mvc.js
+++ js/mvc.js
@@ -1,4 +1,12 @@
 (function($){
+    var Model = $.Model = function(){
+        this.permutation = $.permutation(0, 1, 2, 3, 4, 5, 6, 7, 8);
+        this.rotation = $.permutation(1, 2, 3, 4, 5, 6, 7, 8, 0);
+    };
+    Model.prototype.rotate = function(){
+        this.permutation = this.permutation.multiply(this.rotation);
+    };
+
     var View = $.View = function(model, tiles){
         this.model = model;
         this.tiles = tiles;
--- js/wish.js
+++ js/wish.js
@@ -1,7 +1,6 @@
 (function(wish){
-    var identity = wish.permutation(0, 1, 2, 3, 4, 5, 6, 7, 8);
-    var rotate = wish.permutation(1, 2, 3, 4, 5, 6, 7, 8, 0);
-    var permutation = identity.multiply(rotate);
+    var model = new wish.Model();
+
     var tiles = window.tiles = wish.toArray(document.getElementsByClassName('tile'));
-    new wish.View({permutation: permutation}, tiles);
+    new wish.View(model, tiles);
 })(wish);
