d7ee700820b1381897fc59f11e3ce87cd4dad837
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2015-12-18 06:47:06 +0100

    Change order of tiles

--- js/wish.js
+++ js/wish.js
@@ -3,8 +3,9 @@
         return Array.prototype.slice.call(xs);
     };
 
+    var permutation = [1, 0, 3, 2, 5, 4, 7, 6, 8];
     var tiles = window.tiles = toArray(document.getElementsByClassName('tile'));
     tiles.forEach(function(tile, index){
-        tile.style.order = index;
+        tile.style.order = permutation[index];
     });
 })();
