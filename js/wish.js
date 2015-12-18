f2db96aab8a99b71c6a5e1ab3d96b718c491e410
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2015-12-18 06:43:56 +0100

    Set the order of the tiles

--- js/wish.js
+++ js/wish.js
@@ -1,3 +1,10 @@
 (function(){
-    console.log('Hello, World!');
+    function toArray(xs){
+        return Array.prototype.slice.call(xs);
+    };
+
+    var tiles = window.tiles = toArray(document.getElementsByClassName('tile'));
+    tiles.forEach(function(tile, index){
+        tile.style.order = index;
+    });
 })();
