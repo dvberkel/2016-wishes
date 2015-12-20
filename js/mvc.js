b3d3908ea26f705380fcd007ac5e0946f4efc877
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2015-12-20 10:44:31 +0100

    Remove superfluous field

--- js/mvc.js
+++ js/mvc.js
@@ -30,7 +30,6 @@
     var Model = $.Model = function(){
         Observable.call(this);
         this.permutation = $.permutation(0, 1, 2, 3, 4, 5, 6, 7, 8);
-        this.rotation = $.permutation(1, 2, 3, 4, 5, 6, 7, 8, 0);
     };
     Model.prototype = Object.create(Observable.prototype);
     Model.prototype.constructor = Model;
