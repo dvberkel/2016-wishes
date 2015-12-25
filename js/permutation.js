594dc8c4b425eaff475623e8f83192b9c5bbe59d
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2015-12-25 08:37:10 +0100

    Round product in sign calculation
    Even though product is either 1 or -1, due to floating point arithmetic
    The answer could stray. By rounding we are solving a problem when we
    check for the answer

--- js/permutation.js
+++ js/permutation.js
@@ -18,4 +18,13 @@
             return accumalator && (element == index);
         }, true);
     };
+    Permutation.prototype.sign = function(){
+        var product = 1;
+        for (var i = 0; i < this.image.length; i++) {
+            for (var j = i + 1; j < this.image.length; j++) {
+                product *= (this.image[i] - this.image[j]) / (i - j);
+            }
+        };
+        return Math.round(product);
+    };
 })(window.wish = window.wish || {});
